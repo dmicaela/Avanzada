bdc1458fd7761f6a986f757bba2631a54a225655
Dafne Micaela Terrazas <dmicaelaterrazas@gmail.com>
2024-11-26 09:08:34 -0300

    Paso #1: Funciones

---
+++ parcial02/script1.py
@@ -0,0 +1,10 @@
+""" Sumatoria:
+Crear un script de python que contenga una función con el 
+nombre “sumar”, reciba 2 parámetros y retorne la suma de los dos 
+valores pasados como parámetros.
+"""
+
+def sumar(a, b):
+    return a + b
+
+print(sumar(4, 14))
---
+++ parcial02/script2.py
@@ -0,0 +1,12 @@
+"""Sumatoria de Lista:
+Crear un script de python que contenga una función con el 
+nombre “sumar_lista”, reciba como único parámetros una lista de 
+números y retorne la sumatoria de todos los números de la lista.
+"""
+
+def sumar_lista(lista):
+    return sum(lista)
+
+"""Probamos funcion"""
+print(sumar_lista([1,6,7,3,9,5,8,2,3]))
+print(sumar_lista([5,9]))
